68df9627c59979ffc87048e456eb0f8c01153e16
ryan <ryanvaniderstine@maximus.com>
2016-08-21 09:36:53 -0400

    Picking up latest generated documentation

--- dist/api_project.js
+++ dist/api_project.js
@@ -1,23 +1,1 @@
-define({
-  "name": "DXCIMRCA",
-  "version": "0.9.0",
-  "description": "DXC IMR CA API",
-  "title": "DXC IMR CA API",
-  "url": "http://imr-ca-sandbox.maxird.com/",
-  "header": {
-    "title": "Overview",
-    "content": ""
-  },
-  "footer": {
-    "title": "",
-    "content": "<p>Copyright © 2016, MAXIMUS Inc. All Rights Reserved.</p>\n"
-  },
-  "sampleUrl": false,
-  "apidoc": "0.2.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2016-08-21T05:51:43.223Z",
-    "url": "http://apidocjs.com",
-    "version": "0.16.1"
-  }
-});
+define({  "name": "DXCIMRCA",  "version": "0.9.0",  "description": "DXC IMR CA API",  "title": "DXC IMR CA API",  "url": "http://imr-ca-sandbox.maxird.com/",  "header": {    "title": "Overview",    "content": ""  },  "footer": {    "title": "",    "content": "<p>Copyright © 2016, MAXIMUS Inc. All Rights Reserved.</p>\n"  },  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-08-21T13:34:36.865Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
